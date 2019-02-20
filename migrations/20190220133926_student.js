eef225e4443d3b83c794da3a893460cb6cb8871e
Kingaa6 <Kingaa6@gmail.com>
2019-02-20 13:59:48 -0800

    Add migrations

--- migrations/20190220133926_student.js
+++ migrations/20190220133926_student.js
@@ -1,3 +1,14 @@
-exports.up = function(knex, Promise) {};
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cohort', function(table) {
+    table.increments();
+    table
+      .string('name', 128)
+      .unique()
+      .notNullable();
+    table.timestamp(true, true);
+  });
+};
 
-exports.down = function(knex, Promise) {};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cohort');
+};
